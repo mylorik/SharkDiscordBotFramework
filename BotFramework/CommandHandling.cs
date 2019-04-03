24b93d00ae901aa170b2612ba8a5a7b9bc121401
Artem <35617948+mylorik@users.noreply.github.com>
2019-04-02 23:26:35 -0400

    Update CommandHandling.cs

--- BotFramework/CommandHandling.cs
+++ BotFramework/CommandHandling.cs
@@ -4,11 +4,11 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using King_of_the_Garbage_Hill.BotFramework.Extensions;
-using King_of_the_Garbage_Hill.LocalPersistentData.UsersAccounts;
+using SharkFramework.BotFramework.Extensions;
+using SharkFramework.LocalPersistentData.UsersAccounts;
 using Lamar.IoC;
 
-namespace King_of_the_Garbage_Hill.BotFramework
+namespace SharkFramework.BotFramework
 {
     public sealed class CommandHandling : ModuleBaseCustom, IServiceSingleton
     {
